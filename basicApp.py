ac683e7f10e3d115fea4791ff32692967a4637d3
Mark Richey <mark_richey@outlook.com>
2022-01-27 16:05:08 +0000

    Reads back a user input

--- basicApp.py
+++ basicApp.py
@@ -8,9 +8,25 @@ from kivy.uix.textinput import TextInput
 class SayHello(App):
     def build(self):
         self.window = GridLayout()
-        #add widgets to window
+        
+        self.window.cols = 1
+        self.window.add_widget(Image(source="img.jpg"))
+
+        self.greeting = Label(text="Name Yourself!")
+        self.window.add_widget(self.greeting)
+
+        self.user = TextInput(multiline=False)
+        self.window.add_widget(self.user)
+
+        self.button = Button(text="Submit")
+        self.button.bind(on_press=self.callback)
+        self.window.add_widget(self.button)
 
         return self.window
+
+    def callback(self,instance):
+        self.greeting.text = "Greetings " + self.user.text + "!"
+        return
 
 if __name__ == "__main__":
     SayHello().run()
