2f8c65e5f5ea98ffbd91d0c27dc9772f1e5d0548
Mark Richey <mark_richey@outlook.com>
2022-01-27 16:16:58 +0000

    styling ideas

--- basicApp.py
+++ basicApp.py
@@ -8,17 +8,33 @@ from kivy.uix.textinput import TextInput
 class SayHello(App):
     def build(self):
         self.window = GridLayout()
-        
         self.window.cols = 1
+        self.window.size_hint = (0.6,0.7) # Sides / Top Bot
+        self.window.pos_hint = {"center_x": 0.5,"center_y": 0.5}
+
         self.window.add_widget(Image(source="img.jpg"))
 
-        self.greeting = Label(text="Name Yourself!")
+        self.greeting = Label(
+            text="Name Yourself!",
+            font_size = 18,
+            color = "#EB5131"
+        )
         self.window.add_widget(self.greeting)
 
-        self.user = TextInput(multiline=False)
+        self.user = TextInput(
+            multiline=False,
+            padding_y = (20,20),
+            size_hint = (1,0.5)
+        )
         self.window.add_widget(self.user)
 
-        self.button = Button(text="Submit")
+        self.button = Button(
+            text="Submit",
+            size_hint = (1,0.5),
+            bold = True,
+            background_color = "#EB5131",
+            background_normal = ""
+        )
         self.button.bind(on_press=self.callback)
         self.window.add_widget(self.button)
 
